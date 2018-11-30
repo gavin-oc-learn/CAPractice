1663e0d16b199fa1a889611ff734b843836bf6bd
liukai <liukai@jiduo365.cn>
2018-11-30 14:28:07 +0800

    Initial Commit

---
+++ CAPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CAPractice
+//
+//  Created by liu kai on 2018/11/30.
+//  Copyright © 2018 liu kai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CAPracticeTests/CAPracticeTests.m
@@ -0,0 +1,37 @@
+//
+//  CAPracticeTests.m
+//  CAPracticeTests
+//
+//  Created by liu kai on 2018/11/30.
+//  Copyright © 2018 liu kai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CAPracticeTests : XCTestCase
+
+@end
+
+@implementation CAPracticeTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
